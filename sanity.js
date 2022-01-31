97b5d882e038122fa9528f6d535ff72676b3dc73
farnood lotfali <farnoodlotfali@gmail.com>
2022-01-31 19:08:50 +0330

    finish meduim

---
+++ sanity.js
@@ -0,0 +1,18 @@
+import {
+  createClient,
+  createCurrentUserHook,
+  createImageUrlBuilder,
+} from 'next-sanity'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-03-25',
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (src) => createImageUrlBuilder(config).image(src)
+
+export const useCurrentUser = (src) => createCurrentUserHook(config)
